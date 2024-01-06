a86c424746e78139713b388acce24011ba9cde91
Ayush Raj <ayushraj1268@gmail.com>
2024-01-06 19:53:20 +0530

    Merge commit '60d58210c83065031067326bb72b7d7c34f4d4e0'

--- src/app/api/getMemberAccounts/route.js
+++ src/app/api/getMemberAccounts/route.js
@@ -1,16 +1,37 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import connect from "@/db";
 import Member from "@/models/member";
 
 connect();
-
+/**
+ *
+ * @param {NextRequest} request
+ * @returns
+ */
 export async function GET(request) {
-    return Member.find().then(
-        (members) => {
-            return NextResponse.json({ members });
-        },
-        (error) => {
-            return NextResponse.json({ error });
+  const { searchParams } = request.nextUrl;
+  const membershipNumber = searchParams.get("membershipNumber");
+  if (membershipNumber) {
+    return Member.findOne({ membershipNumber }).then(
+      (member) => {
+        if (!member) {
+          return NextResponse.json({
+            errorMessage: "Invalid membership number",
+          });
         }
+        return NextResponse.json({ member });
+      },
+      (error) => {
+        return NextResponse.json({ error });
+      }
     );
+  }
+  return Member.find().then(
+    (members) => {
+      return NextResponse.json({ members });
+    },
+    (error) => {
+      return NextResponse.json({ error });
+    }
+  );
 }
